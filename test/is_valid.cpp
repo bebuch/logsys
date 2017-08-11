ab9de1d909c16200f00bc8a4adf2d1e53b0dcfd2
Benjamin Roland Buch <benni.buch@gmail.com>
2017-08-11 20:20:58 +0200

    unit test with lvalue's

--- test/is_valid.cpp
+++ test/is_valid.cpp
@@ -5,23 +5,23 @@ struct B{ int v; };
 struct C{ void f(){} };
 
 int main(){
-	static_assert(!logsys::detail::is_valid< A >([](auto&& x)->decltype(
+	static_assert(!logsys::detail::is_valid< A >([](auto& x)->decltype(
 		(void)x.v
 	){}));
-	static_assert(logsys::detail::is_valid< B >([](auto&& x)->decltype(
+	static_assert(logsys::detail::is_valid< B >([](auto& x)->decltype(
 		(void)x.v
 	){}));
-	static_assert(!logsys::detail::is_valid< C >([](auto&& x)->decltype(
+	static_assert(!logsys::detail::is_valid< C >([](auto& x)->decltype(
 		(void)x.v
 	){}));
 
-	static_assert(!logsys::detail::is_valid< A >([](auto&& x)->decltype(
+	static_assert(!logsys::detail::is_valid< A >([](auto& x)->decltype(
 		(void)x.f()
 	){}));
-	static_assert(!logsys::detail::is_valid< B >([](auto&& x)->decltype(
+	static_assert(!logsys::detail::is_valid< B >([](auto& x)->decltype(
 		(void)x.f()
 	){}));
-	static_assert(logsys::detail::is_valid< C >([](auto&& x)->decltype(
+	static_assert(logsys::detail::is_valid< C >([](auto& x)->decltype(
 		(void)x.f()
 	){}));
 }
